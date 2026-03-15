1974a9680271362cfa16127f10e56fb45f7920a7
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-03-15 23:14:33 +0000

    Simplify recipe detail page layout

--- accounts/models.py
+++ accounts/models.py
@@ -3,10 +3,6 @@ from django.db import models
 
 
 class Profile(models.Model):
-    """
-    Modèle de profil utilisateur étendu.
-    Chaque utilisateur possède un profil avec une bio et une photo.
-    """
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(max_length=500, blank=True, verbose_name='Biographie')
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='Avatar')
